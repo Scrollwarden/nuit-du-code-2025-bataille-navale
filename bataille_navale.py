a85d749738c716ffe0a77dc952c22ea847e760bd
Kleirre <claire.oudot.lyon@gmail.com>
2025-05-14 17:47:24 +0200

    Add files via upload

--- bataille_navale.py
+++ bataille_navale.py
@@ -3,8 +3,8 @@ import pyxel
 pyxel.init(256, 256, title="Bataille navale")
 
 pyxel.load("theme.pyxres")
-
-
+fleche_feu_j1 =False
+fleche_feu_j2 =True
 
 def update():
     """mise à jour des variables (30 fois par seconde)"""
@@ -18,9 +18,14 @@ def draw():
     for ligne in range(2) :
         for elt in range(4):
             pyxel.blt(4 + 25 * elt,2 + 40 * ligne,0,88 +16*elt,0 + 16*ligne,16,16)
+    if fleche_feu_j1 == True :
+        pyxel.blt(79,2,0,152,0,16,16)
 
     for ligne in range(2) :
         for elt in range(4):
-            pyxel.blt(235 - 25 * elt,235  - 40 * ligne,0,152 -16*elt,32 - 16*ligne,16,16)
+            pyxel.blt(160+ 25 * elt,170  +40 * ligne,0,88 +16*elt,0 + 16*ligne,16,16)
+    if fleche_feu_j2 == True :
+        pyxel.blt(235,170,0,152,0,16,16)
+
 
 pyxel.run(update, draw)
