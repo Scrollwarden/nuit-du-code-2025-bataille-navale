22026fb66fecbf2dd1d8e3586f5c71f57421c483
xXMulticolorXx <titouanmarty8@gmail.com>
2025-05-14 15:01:54 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,3 +1,36 @@
 '''
-
+Classe jeu
 '''
+import pyxel as px
+import Navire
+
+
+class Jeu:
+     
+    def __init__(self):
+
+        self.navire1 = Navire()
+        self.navire2 = Navire()
+     
+    def update(self):
+        """met à jour le jeu"""
+        
+        self.navire1.update()
+        self.navire2.update()
+    
+    def draw(self):
+        """
+        Efface l'écran et dessine la mer
+        """
+        px.cls(5)
+        self.navire1.draw()
+        self.navire2.draw()
+        
+
+
+
+if __name__ == "__main__":
+    # Démarre l'application
+    px.init(80,80, title="Bataille Navale", fps = 60)
+    appli = Jeu()
+    px.run(appli.update, appli.draw)
