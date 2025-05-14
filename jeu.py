e9e75abda454b76932af26b48be4af10ff60fe35
xXMulticolorXx <titouanmarty8@gmail.com>
2025-05-14 16:33:00 +0200

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -11,55 +11,32 @@ class Jeu:
         """crée deux navires"""
         self.navire1 = Navire(3)
         self.navire2 = Navire(7)
-        self.projectile1 ='flèche'
-        self.projectile2 = 'flèche'
      
     def update(self):
         """met à jour le jeu"""
         
         if px.btnp(px.KEY_A):
-            if self.projectile1 == 'flèche':
-                self.projectile1 = 'feu'
-            else:
-                self.projectile1 = 'flèche'
+            self.navire1.change_type_fleche()
                 
         if px.btnp(px.KEY_E):
             if :
-                type_degats , degats = self.navire1.inflige_degats(self.projectile1)
+                type_degats , degats = self.navire1.inflige_degats(self.navire1.type_fleche)
                 self.navire2.prends_degats(type_degats, degats)
                 
         if px.btnp(px.KEY_0):
-            if self.projectile2 == 'flèche':
-                self.projectile2 = 'feu'
-            else:
-                self.projectile2 = 'flèche'
+            self.navire2.change_type_fleche()
                 
         if px.btnp(px.KEY_1):
             if :
-                type_degats , degats = self.navire2.inflige_degats(self.projectile2)
+                type_degats , degats = self.navire2.inflige_degats(self.navire2.type_fleche)
                 self.navire1.prends_degats(type_degats, degats)
         
-        if px.btnp(px.KEY_Z):
-            if px.btnp(px.KEY_Q):
-                self.navire1.change_orientation(8)
-            elif px.btnp(px.KEY_D):
-                self.navire1.change_orientation(2)
-            else:
-                self.navire1.change_orientation(1)
-                
-        elif px.btnp(px.KEY_S):
-            if px.btnp(px.KEY_Q):
-                self.navire1.change_orientation(6)
-            elif px.btnp(px.KEY_D):
-                self.navire1.change_orientation(4)
-            else:
-                self.navire1.change_orientation(5)
-                
-        elif px.btnp(px.KEY_Q):
-            self.navire1.change_orientation(7)
+        
+        if px.btnp(px.KEY_Q):
+            self.navire1.change_orientation(1)
             
         elif px.btnp(px.KEY_D):
-            self.navire1.change_orientation(3)
+            self.navire1.change_orientation(-1)
             
         if px.btn(px.KEY_SHIFT):
             self.navire1.change_vitesse(True)
@@ -68,27 +45,12 @@ class Jeu:
             self.navire1.change_vitesse(False)
             
             
-        if px.btnp(px.KEY_UP):
-            if px.btnp(px.KEY_LEFT):
-                self.navire2.change_orientation(8)
-            elif px.btnp(px.KEY_RIGHT):
-                self.navire2.change_orientation(2)
-            else:
-                self.navire2.change_orientation(1)
-                
-        elif px.btnp(px.KEY_DOWN):
-            if px.btnp(px.KEY_LEFT):
-                self.navire2.change_orientation(6)
-            elif px.btnp(px.KEY_RIGHT):
-                self.navire2.change_orientation(4)
-            else:
-                self.navire2.change_orientation(5)
-                
-        elif px.btnp(px.KEY_LEFT):
-            self.navire2.change_orientation(7)
+        
+        if px.btnp(px.KEY_LEFT):
+            self.navire2.change_orientation(1)
             
         elif px.btnp(px.KEY_RIGHT):
-            self.navire2.change_orientation(3)
+            self.navire2.change_orientation(-1)
         
         if px.btn(px.KEY_PLUS):
             self.navire2.change_vitesse(True)
